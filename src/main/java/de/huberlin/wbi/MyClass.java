f6cb6ce533056c2391165a383272b39d87dfbd5e
Marc Bux <buxmarcn@informatik.hu-berlin.de>
2016-11-03 15:56:49 +0100

    initial commit

---
+++ src/main/java/de/huberlin/wbi/MyClass.java
@@ -0,0 +1,25 @@
+package de.huberlin.wbi;
+
+import java.util.Collection;
+
+public class MyClass {
+
+    /**
+     * A short little method that tests of an element is contained in a
+     * collection.
+     * 
+     * @param collection
+     *            the collection that is being searched
+     * @param element
+     *            the element that is to be searched
+     * @return true, if and only if the element is contained in the collection
+     */
+    public static <T> boolean contains(Collection<T> collection, T element) {
+	for (T t : collection) {
+	    if (t.equals(element))
+		return true;
+	}
+	return false;
+    }
+
+}
