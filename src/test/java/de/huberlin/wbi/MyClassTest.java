507bbf2951088067b45e3842ba5149a6b3af2b49
Marc Bux <buxmarcn@informatik.hu-berlin.de>
2016-11-03 17:47:11 +0100

    fixed tests to invoke non-static method

--- src/test/java/de/huberlin/wbi/MyClassTest.java
+++ src/test/java/de/huberlin/wbi/MyClassTest.java
@@ -10,23 +10,25 @@ import org.junit.Test;
 
 public class MyClassTest {
 
+    @SuppressWarnings("static-method")
     @Test
     public void testContains() {
 	Collection<Integer> list = Arrays.asList(5, 2, 7, 8);
+	MyClass myClass = new MyClass();
 	
-	assertTrue(MyClass.contains(list, 2));
-	assertFalse(MyClass.contains(list, 4));
+	assertTrue(myClass.contains(list, 2));
+	assertFalse(myClass.contains(list, 4));
 	
-	assertTrue(MyClass.contains(list, 5));
-	assertTrue(MyClass.contains(list, 8));
-	assertTrue(MyClass.contains(Arrays.asList(5), 5));
-	assertFalse(MyClass.contains(new ArrayList<Integer>(), 5));
+	assertTrue(myClass.contains(list, 5));
+	assertTrue(myClass.contains(list, 8));
+	assertTrue(myClass.contains(Arrays.asList(5), 5));
+	assertFalse(myClass.contains(new ArrayList<Integer>(), 5));
 	
-	assertFalse(MyClass.contains(null, 5));
-	assertFalse(MyClass.contains(list, null));
-	assertFalse(MyClass.contains(null, null));
-	assertTrue(MyClass.contains(Arrays.asList(5, null, 4, 3), 4));
-	assertTrue(MyClass.contains(Arrays.asList(5, null, 4, 3), null));
+	assertFalse(myClass.contains(null, 5));
+	assertFalse(myClass.contains(list, null));
+	assertFalse(myClass.contains(null, null));
+	assertTrue(myClass.contains(Arrays.asList(5, null, 4, 3), 4));
+	assertTrue(myClass.contains(Arrays.asList(5, null, 4, 3), null));
     }
 
 }
