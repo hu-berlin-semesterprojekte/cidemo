40346246410d6feabf1dfd6f9413427b08aa8a51
Patrick Schaefer <patrick.schaefer@hu-berlin.de>
2018-11-04 16:15:31 +0100

    testcase

---
+++ src/main/java/de/huberlin/wbi/Player.java
@@ -0,0 +1,50 @@
+package de.huberlin.wbi;
+
+public class Player {
+
+  public static enum PassengerColor {
+    Blue, Black, Red, Rainbow
+  }
+
+  int[] playerCards = new int[4];
+
+  /**
+   * Claim a route between two adjacent cities using
+   * the payByColor-passenger-cards in our hand
+   * @param payByColor
+   * @param routeCost
+   * @param routeColor
+   * @return
+   */
+  public boolean claimRoute(
+          PassengerColor payByColor,
+          int routeCost,
+          PassengerColor routeColor
+      ) {
+    int currentCards = playerCards[payByColor.ordinal()];
+    int rainBowCards = playerCards[PassengerColor.Rainbow.ordinal()];
+
+    // Pay for a route between two adjacent cities on the map
+    if ( (payByColor == routeColor
+            || payByColor == PassengerColor.Rainbow
+            || routeColor == PassengerColor.Rainbow)) {
+      // no rainbow cards needed
+      if (currentCards >= routeCost) {
+        playerCards[payByColor.ordinal()] -= routeCost;
+        return true;
+      }
+      // rainbox cards are needed
+      else if (currentCards + rainBowCards >= routeCost){
+        playerCards[payByColor.ordinal()]= 0;
+        playerCards[PassengerColor.Rainbow.ordinal()] -= (routeCost - currentCards);
+        return true;
+      }
+    }
+
+    // we cannot by the route
+    return false;
+
+  }
+
+
+}
---
+++ src/test/java/de/huberlin/wbi/PlayerTest.java
@@ -0,0 +1,37 @@
+package de.huberlin.wbi;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PlayerTest {
+
+  @Test
+  public void testContains() {
+
+    Player p = new Player();
+    Arrays.fill(p.playerCards, 3);
+
+    assertTrue(p.claimRoute(Player.PassengerColor.Black, 1, Player.PassengerColor.Black));
+    assertTrue(p.claimRoute(Player.PassengerColor.Red, 1, Player.PassengerColor.Rainbow));
+    assertTrue(p.claimRoute(Player.PassengerColor.Rainbow, 1, Player.PassengerColor.Blue));
+
+    assertFalse(p.claimRoute(Player.PassengerColor.Black, 2, Player.PassengerColor.Blue));
+    assertFalse(p.claimRoute(Player.PassengerColor.Red, 10, Player.PassengerColor.Red));
+
+    // pay by rainbow
+    assertTrue(p.claimRoute(Player.PassengerColor.Black, 3, Player.PassengerColor.Black));
+
+    // double spend
+    // assertTrue(p.claimRoute(Player.PassengerColor.Rainbow, 2, Player.PassengerColor.Red));
+
+    //     Blue, Black, Red, Rainbow
+    System.out.println(Arrays.toString(p.playerCards));
+    for (int c : p.playerCards) {
+      assertTrue(c >= 0);
+    }
+  }
+}
